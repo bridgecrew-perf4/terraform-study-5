365594e1aecda782f050b01e00fc92165f2b08ab
Yuri Souza Santos <yurisouza1712@gmail.com>
2021-02-07 14:50:10 -0300

    adicionando o datasources e ajustando ec2

--- ec2.tf
+++ ec2.tf
@@ -1,26 +1,15 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  owners = ["099720109477"] # Ubuntu
-}
-
 resource "aws_instance" "web" {
-  ami           = var.image_id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   subnet_id     = "subnet-025eb5dcb52def1e3" # subnet existente na VPC default da região us-east-1
-#  key_name      = "<arquivo.pem>"
+  #  key_name      = "<arquivo.pem>"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "terraform-study"
   }
 }
 
 resource "aws_eip" "ip" {
-  vpc = true
+  vpc      = true
   instance = "aws_instance.web.id"
 }
